6fc91b9509de705a18f1c14a63c1e6cf787ae286
sojeda <sojeda@funcionasi.es>
2018-09-03 16:14:15 +0200

    todo funciona

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { RoomPage } from '../room/room';
 import firebase from 'firebase';
+import { PushNotificationProvider } from '../../providers/push-notification/push-notification';
 
 /**
  * Generated class for the SigninPage page.
@@ -13,45 +14,53 @@ import firebase from 'firebase';
 @IonicPage()
 @Component({
   selector: 'page-signin',
-  templateUrl: 'signin.html',
+  templateUrl: 'signin.html'
 })
 export class SigninPage {
   public data = { nickname: '', password: '' };
 
   public dataRegister = {
-    email:'',
-    password:''
-  }
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-
-  }
+    email: '',
+    password: ''
+  };
+  constructor(public navCtrl: NavController, public navParams: NavParams, public pushNotification: PushNotificationProvider) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SigninPage');
   }
- 
-  enterNickname() {
-    firebase.auth().signInWithEmailAndPassword(this.data.nickname, this.data.password).then(data => { 
-      this.navCtrl.setRoot(RoomPage, {
-        nickname: this.data.nickname,
-        userLoggedData : data
-      });
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ...
-    });
 
+  enterNickname() {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(this.data.nickname, this.data.password)
+      .then(data => {
+        this.navCtrl.setRoot(
+          RoomPage,
+          {
+            nickname: this.data.nickname,
+            userLoggedData: data
+          }          
+        );
+        this.pushNotification.getDeviceInfo();
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
   }
-  public createUser() { 
+  public createUser() {
     let email = this.dataRegister.email;
-    let password = this.dataRegister.password
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ...
-    });
+    let password = this.dataRegister.password;
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
   }
 }
--- src/providers/push-notification/push-notification.ts
+++ src/providers/push-notification/push-notification.ts
@@ -1,18 +1,12 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Push, PushObject, PushOptions } from '@ionic-native/push';
 import { ToastController } from 'ionic-angular';
 import { Device } from '@ionic-native/device';
 
-/*
-  Generated class for the PushNotificationProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class PushNotificationProvider {
-  constructor(public http: HttpClient, private push: Push, private device: Device, private toastCtrl: ToastController) {
+  constructor( private push: Push, private device: Device, private toastCtrl: ToastController) {
     this.push.hasPermission().then((res: any) => {
       if (res.isEnabled) {
         console.log('We have permission to send push notifications');
